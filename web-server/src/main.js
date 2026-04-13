a485f070d66d50a0b025351fa08a9283e387ea8a
Damir Rashidov <doomfinland@gmail.com>
2026-04-14 01:13:20 +0300

    Full release

--- web-server/src/main.js
+++ web-server/src/main.js
@@ -1,44 +1,60 @@
 import payload from "./proto.js";
-import { encode, decode } from "wasm-js";
+import { encode as jsEncode, decode as jsDecode } from "wasm-js";
+import init, { encode as rustEncode, decode as rustDecode, hello_user } from "wasm-rs";
 import "./style.css";
 
 const Payload = payload.main.Payload;
 
 let isBenchRunning = false;
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+    await init();
+
     const input = document.getElementById("mainInput");
     const button = document.getElementById("acceptButton");
     const output = document.getElementById("testOutput");
+    const select = document.getElementById("wasmSelect");
 
     input.addEventListener("input", () => handleInput(input));
     button.addEventListener("click", async () => {
-        await handleButtonPress(input, output);
+        await handleButtonPress(input, output, select);
     });
+    
+    console.log("All loaded successfully!");
 });
 
 const handleInput = (input) => {
     input.value = input.value.replace(/[^0-9]/g, "");
 };
 
-const handleButtonPress = async (input, output) => {
-    if (!input.value.trim() || isBenchRunning) return;
+const handleButtonPress = async (input, output, select) => {
+    if (!input.value.trim() || !select.value.trim() || isBenchRunning) return;
 
     isBenchRunning = true;
     output.innerText = "Running...";
 
     const count = Number(input.value);
-    const result = benchmark(count);
+    const type = select.value.trim();
+    const result = benchmark(count, type);
 
-    output.innerText = `${result.toFixed(3)} ms`;
+    output.innerText = `${result} ms`;
     isBenchRunning = false;
 };
 
-function benchmark(count) {
-    const t0 = performance.now();
-    const buf = encode(Payload, count);
-    decode(Payload, buf);
-    const t1 = performance.now();
+function benchmark(count, type) {
+    if (type == "js") {
+        const t0 = performance.now();
+        const buf = jsEncode(Payload, count);
+        jsDecode(Payload, buf);
+        const t1 = performance.now();
 
-    return t1 - t0;
+        return t1 - t0;
+    } else if (type == "rust") {
+        const t0 = performance.now();
+        const buf = rustEncode(count);
+        rustDecode(buf);
+        const t1 = performance.now();
+
+        return t1 - t0;
+    }
 }
